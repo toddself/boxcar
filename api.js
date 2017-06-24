60f54e2f1a5b62bc3f6fb9ac7e6646db051f74e4
Todd Kennedy <todd@selfassembled.org>
2017-06-24 12:29:14 -0700

    [record scratch]

--- api.js
+++
@@ -1,9 +0,0 @@
-const outbound = require('./lib/notify').outbound
-const inbound = require('./lib/notify').inbound
-
-outbound.insertRow = (evt) => inbound({type: 'insertRow', id: evt.id, rows: evt.rows})
-outbound.deleteRow = (evt) => inbound({type: 'removeRow', id: evt.id})
-outbound.update = (evt) => inbound({type: 'updateCell', cell: evt.cell, data: evt.data})
-outbound.userActive = (evt) => inbound({type: 'userActive', cell: evt.cell})
-
-module.exports = outbound
